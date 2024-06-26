cbc2a6ad27b9c75eb4f0d2ea40e92d1c848a6e6b
QuanDH <rickymta@gmail.com>
2024-06-26 16:42:34 +0700

    Remove and rebuild

--- src/Core/Draft.Core.Infrastructure/InfrastructureRegister.cs
+++ src/Core/Draft.Core.Infrastructure/InfrastructureRegister.cs
@@ -17,16 +17,12 @@ public static class InfrastructureRegister
     {
         services.AddMemoryCache();
         services.AddHttpContextAccessor();
-        services.AddTransient<IAdminApiProvider, AdminApiProvider>();
         services.AddTransient<IConfigProvider, ConfigProvider>();
         services.AddTransient<ICookieProvider, CookieProvider>();
-        services.AddTransient<ICoreApiProvider, CoreApiProvider>();
-        services.AddTransient<ICustomerApiProvider, CustomerApiProvider>();
         services.AddTransient<IHashProvider, HashProvider>();
         services.AddTransient<IJwtProvider, JwtProvider>();
         services.AddTransient<ILogProvider, LogProvider>();
         services.AddTransient<IMemCacheProvider, MemCacheProvider>();
-        services.AddTransient<IRestProvider, RestProvider>();
         services.AddTransient<IStringProvider, StringProvider>();
     }
 }
